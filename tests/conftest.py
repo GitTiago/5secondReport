229470b8fd9b14ef56119eec0597e6b0d414f6ee
Tiago <tiagomanuelfs@hotmail.com>
2021-05-02 19:07:34 +0100

    Add test for server endpoint
    Checks formatting on line and correct file name creation

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,10 @@
 import asyncio
+from typing import Generator
+from fastapi.testclient import TestClient
 
 import pytest
+
+from server import app
 
 
 @pytest.fixture(scope="session")
@@ -12,3 +16,9 @@ def event_loop():
     loop = asyncio.get_event_loop()
     yield loop
     loop.close()
+
+
+@pytest.fixture(scope="module")
+def client() -> Generator:
+    with TestClient(app) as c:
+        yield c
---
+++ tests/test_server.py
@@ -0,0 +1,38 @@
+from fastapi.testclient import TestClient
+from datetime import datetime
+from http import HTTPStatus
+
+import os
+
+TEST_CLIENT_ID = "TEST_CLIENT"
+EXPECTED_DATE_FORMAT = "%m/%d/%Y, %H:%M:%S"
+
+
+def test_post_programs_success(client: TestClient):
+    test_beginning_timestamp = datetime.now()
+    file_path = f"{TEST_CLIENT_ID}_running_programs.txt"
+    assert not os.path.exists(file_path)
+
+    try:
+        test_programs = "a.exe,b.exe,4.exe"
+
+        response = client.post(f"/programs/{TEST_CLIENT_ID}", json=["a.exe,b.exe,4.exe"])
+        assert response.status_code == HTTPStatus.CREATED
+        assert os.path.exists(file_path)
+
+        with open(file_path, mode='r') as programs_file:
+            n_lines = 0
+            for line in programs_file:
+                n_lines += 1
+                end_date_index = line.index(']')
+                date_string = line[1:end_date_index]
+                # By attempting to create a date, we check the formatting
+                line_time = datetime.strptime(date_string,
+                                              EXPECTED_DATE_FORMAT)
+                assert test_beginning_timestamp.replace(microsecond=0) <= line_time
+                assert line_time <= datetime.now().replace(microsecond=0)
+                assert line[end_date_index+1:].strip() == test_programs
+            assert n_lines == 1
+    finally:
+        if os.path.exists(file_path):
+            os.remove(file_path)
